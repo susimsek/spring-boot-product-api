1c6b53822534896f29ceb51170bfd7e04a6d9e98
suayb <suaybsimsek58@gmail.com>
2019-04-17 18:00:18 +0300

    User Repository eklendi

---
+++ src/main/java/com/dao/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+import com.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
